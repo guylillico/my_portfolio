b7a9e2f5cc6e0bbcd8db92fba7023a385ff6550d
Guy Lillico <guylillico@gmail.com>
2022-07-21 20:32:42 +0800

    Removed unused sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import BlogPost from "../components/blogPost"
+// import BlogPost from "../components/blogPost"
 import About from "../components/homedefault/about"
 import Banner from "../components/homedefault/banner"
 import Project from "../components/homedefault/project"
 import Service from "../components/homedefault/service"
-import Testimonial from "../components/homedefault/testimonial"
-import Timeline from "../components/homedefault/timeline"
+// import Testimonial from "../components/homedefault/testimonial"
+// import Timeline from "../components/homedefault/timeline"
 import Layout from "../components/layout"
 import SeoComponent from "../components/seo"
 import Contact from "../elements/contact/contact"
@@ -18,10 +18,10 @@ const IndexPage = () => (
     <Service />
     <div id="portfolio">
       <Project />
-      <Testimonial />
+      {/* <Testimonial /> */}
     </div>
-    <Timeline />
-    <BlogPost />
+    {/* <Timeline /> */}
+    {/* <BlogPost /> */}
     <Contact />
   </Layout>
 )
